48bfaa8f28d236a0461a1f20152945dfb455025e
RomanPasichnyu <romjkea@gmail.com>
2024-08-05 21:44:36 +0300

    easy-in

---
+++ homework-1/main.js
@@ -0,0 +1,27 @@
+
+const q = 'hello';
+const w = 'owu';
+const e = 'com';
+const r = 'ua';
+const t = 1;
+const y = 10
+const u = -999;
+const i = 3.14;
+const o = 2.7;
+const p = 16;
+const d = true;
+const f = false
+console.log(q, w, e, r, t, y, u, i, o, p, d, f)
+
+const firstName = `Romjke`;
+const middleName = `Pasichnuy`;
+const lastName = `Kokosovych`;
+const person = console.log(`${middleName} ${firstName} ${lastName}`);
+
+const a = 100;
+const b = '100';
+const c = true;
+
+console.log(typeof a, typeof b, typeof c);
+
+console.log(prompt('Enter your name', 'Romjke'), prompt('Enter your middle name', 'Pasichnuy'), prompt('Enter your age', '12'));
